83953e71dc0fb386bf4cf1fc736f68e128194e24
Konstantin <105041449+konstantin-a-maslov@users.noreply.github.com>
2022-08-18 17:28:14 +0200

    Add files via upload

---
+++ dlmodels/deeplab.py
@@ -0,0 +1,62 @@
+"""
+The implementation is based on https://keras.io/examples/vision/deeplabv3_plus/ with minor modifications
+Note that original DeepLabV3+ uses ResNet-101 or Xception as encoders, not ResNet-50
+"""
+import tensorflow.keras.layers as layers
+import tensorflow.keras.models as models
+import tensorflow.keras.applications as applications
+from dlmodels.unet import conv
+
+
+def conv_block(x, n_filters=256, kernel_size=3, dilation_rate=1, batch_norm=True):
+    y = conv(n_filters, kernel_size, dilation_rate)(x)
+    if batch_norm:
+        y = layers.BatchNormalization()(y)
+    y = layers.LeakyReLU()(y)
+    return y
+
+
+def DilatedSpatialPyramidPooling(dspp_input):
+    dims = dspp_input.shape
+    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(dspp_input)
+    x = conv_block(x, kernel_size=1)
+    out_pool = layers.UpSampling2D(
+        size=(dims[-3] // x.shape[1], dims[-2] // x.shape[2]), interpolation="bilinear",
+    )(x)
+
+    out_1 = conv_block(dspp_input, kernel_size=1)
+    out_6 = conv_block(dspp_input, dilation_rate=6)
+    out_12 = conv_block(dspp_input, dilation_rate=12)
+    out_18 = conv_block(dspp_input, dilation_rate=18)
+
+    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
+    output = conv_block(x, kernel_size=1)
+    return output
+
+
+def deeplab(input_shape, n_classes):
+    inputs = layers.Input(input_shape)
+    resnet50 = applications.ResNet50(
+        weights=None, include_top=False, input_tensor=inputs
+    )
+    x = resnet50.get_layer("conv4_block6_2_relu").output
+    x = DilatedSpatialPyramidPooling(x)
+
+    input_a = layers.UpSampling2D(
+        size=(image_size // 4 // x.shape[1], image_size // 4 // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+
+    input_b = resnet50.get_layer("conv2_block3_2_relu").output
+    input_b = conv_block(input_b, n_filters=48, kernel_size=1)
+
+    x = layers.Concatenate(axis=-1)([input_a, input_b])
+    x = conv_block(x)
+    x = conv_block(x)
+    x = layers.UpSampling2D(
+        size=(image_size // x.shape[1], image_size // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+
+    model_output = layers.Conv2D(n_classes, kernel_size=1, padding="same")(x)
+    return models.Model(inputs=inputs, outputs=model_output)
